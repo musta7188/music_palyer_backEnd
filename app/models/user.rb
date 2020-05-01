d1efb4cd2d39d25f0ac9a3eeb13000d44dcecb4e
musta7188 <musta7188@hotmail.it>
2020-05-01 14:07:33 +0100

    relatiosn model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
- has_secure_password
  has_many :playlists
+ has_many :songs, through: :playlists
+has_secure_password
 end
  
--- db/schema.rb
+++ db/schema.rb
@@ -16,20 +16,17 @@ ActiveRecord::Schema.define(version: 2020_04_30_112735) do
   enable_extension "plpgsql"
 
   create_table "playlist_songs", force: :cascade do |t|
-    t.bigint "playlist_id", null: false
-    t.bigint "song_id", null: false
+    t.integer "playlist_id"
+    t.integer "song_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["playlist_id"], name: "index_playlist_songs_on_playlist_id"
-    t.index ["song_id"], name: "index_playlist_songs_on_song_id"
   end
 
   create_table "playlists", force: :cascade do |t|
     t.string "name"
-    t.bigint "user_id", null: false
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_playlists_on_user_id"
   end
 
   create_table "songs", force: :cascade do |t|
@@ -50,7 +47,4 @@ ActiveRecord::Schema.define(version: 2020_04_30_112735) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "playlist_songs", "playlists"
-  add_foreign_key "playlist_songs", "songs"
-  add_foreign_key "playlists", "users"
 end
